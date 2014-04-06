d9535c69f4879c5d38facd88657a98f7a8409a5c
itayw <itay@joo.la>
2014-04-06 11:46:40 +0300

    #57 setup of initial test suite.

--- test/unit/4_dispatch/dispatch.spec.js
+++ test/unit/4_dispatch/dispatch.spec.js
@@ -53,41 +53,4 @@ describe("dispatch", function () {
       done();
     });
   });
-
-  it("should blacklist an ip", function (done) {
-    joola.system.blacklist('255.255.255.255', true, 0, function (err) {
-      if (err)
-        return done(err);
-
-      done();
-    });
-  });
-
-  it("should remove blacklisted ip", function (done) {
-    joola.system.blacklist('255.255.255.255', false, 0, function (err) {
-      if (err)
-        return done(err);
-
-      done();
-    });
-  });
-
-  it("should blacklist an ip with ttl", function (done) {
-    joola.system.blacklist('255.255.255.255', true, 1000, function (err) {
-      if (err)
-        return done(err);
-
-      //TODO: Check expired
-      done();
-    });
-  });
-
-  it("should purge the cache", function (done) {
-    joola.system.purgeCache(function (err) {
-      if (err)
-        return done(err);
-
-      done();
-    });
-  });
 });
