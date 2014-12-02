83514eb6302878b2a342b418b6defc0e041e1600
Itay Weinberger <itay@joo.la>
2014-12-02 18:07:04 +0200

    #154 added try/catch on back fill.

--- build/release/joola.js
+++ build/release/joola.js
@@ -25604,12 +25604,13 @@ proto.makeChartTimelineSeries = function (message) {
             _date.setHours(_date.getHours() - (_date.getTimezoneOffset() / 60));
             return new Date(document.values[timestampDimension.key]).getTime() === _date.getTime();
           default:
-            console.log(dimension, document.values[timestampDimension.key], _date);
+            console.log(timestampDimension, document.values[timestampDimension.key], _date);
             return new Date(document.values[timestampDimension.key]).getTime() === _date.getTime();
         }
       }
       catch (ex) {
-        //console.log('exception while checkExists', ex);
+        console.log('exception while checkExists', ex);
+        return;
         //console.log('details', interval, document, documents);
       }
     });
