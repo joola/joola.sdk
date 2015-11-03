28dd2a685b6ee1e7aef2f5b6357cdc95658ab455
Itay Weinberger <itay@joo.la>
2015-11-03 09:43:19 +0200

    #181 added support for local timezone.

--- build/temp/joola.js
+++ build/temp/joola.js
@@ -33632,20 +33632,21 @@ module.exports = exports = _events;
 
 var joola = require('../index');
 
-global.emptyfunc = function () {
+global.emptyfunc = function() {
 
 };
 
-joola.timezone = function (tz) {
+joola.timezone = function(tz) {
   if (tz)
     joola.options.timezoneOffset = tz;
 
   var offset = 0;
-  if (joola.options.timezoneOffset)
-    offset = joola.options.timezoneOffset || (new Date().getTimezoneOffset() / 60 * -1);
+  //if (joola.options.timezoneOffset)
+  offset = joola.options.timezoneOffset || (new Date().getTimezoneOffset() / 60 * -1);
 
   return offset;
 };
+
 }).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"../index":108}],104:[function(require,module,exports){
 (function (Buffer){
@@ -35841,9 +35842,6 @@ var DatePicker = module.exports = function (options, callback) {
   this.base_fromdate.setMinutes(0);
   this.base_fromdate.setSeconds(0);
   this.base_fromdate.setMilliseconds(0);
-
-console.log(this.base_todate, options.daysback);
-console.log(this.base_fromdate);
 
   if (this.base_fromdate < this.min_date) {
     this.base_fromdate = new Date();//this.min_date.fixDate(true, false);
@@ -41509,71 +41507,76 @@ viz.Timeline = require('./Timeline');
 //onscreen
 viz.onscreen = [];
 
-viz.markContainer = function (container, attr) {
+viz.markContainer = function(container, attr) {
   container = $$(container);
   container.attr('jio-domain', 'joola');
   attr = attr.attr || attr;
-  attr.forEach(function (a) {
-    Object.keys(a).forEach(function (key) {
+  attr.forEach(function(a) {
+    Object.keys(a).forEach(function(key) {
       if (key === 'css' && a[key]) {
         var currentClasses = container.attr('class');
         container.attr('class', currentClasses + ' ' + a[key]);
-      }
-      else
+      } else
         container.attr('jio-' + key, a[key]);
     });
   });
 };
 
-viz.initialize = function (self, options, callback) {
+viz.initialize = function(self, options, callback) {
   joola.common.mixin(self.options, options, true);
   var verified = self.verify.apply(self);
   if (verified !== null)
     throw new Error(verified);
   if (!self.initialized) {
-    viz.markContainer(self.options.container, [
-      {type: self.type},
-      {uuid: self.uuid},
-      {css: self.options.css || null}
-    ]);
+    viz.markContainer(self.options.container, [{
+      type: self.type
+    }, {
+      uuid: self.uuid
+    }, {
+      css: self.options.css || null
+    }]);
     self.options.$container = $$(self.options.container);
     if (self.options.canvas) {
       self.options.canvas.addVisualization(self);
       //subscribe to default events
       self.options.canvas.setMaxListeners(100);
-      self.options.canvas.on('datechange', function (dates) {
+      self.options.canvas.on('datechange', function(dates) {
         if (!self.options)
           return;
         if (!self.options.query)
           return;
         //let's change our query and fetch again
         self.options.query = ce.clone(self.options.canvas.prepareQuery(self.options.query[0], dates));
-        viz.destroy(self, {loading: true});
+        viz.destroy(self, {
+          loading: true
+        });
         return viz.initialize(self, self.options);
       });
 
-      self.options.canvas.on('filterchange', function () {
+      self.options.canvas.on('filterchange', function() {
         if (!self.options)
           return;
         if (!self.options.query)
           return;
 
-        self.options.query.forEach(function (query) {
+        self.options.query.forEach(function(query) {
           if (!query.filters)
             query.filter = [];
-          self.options.canvas.options.filters.forEach(function (f) {
-            f.filters.forEach(function (f2) {
+          self.options.canvas.options.filters.forEach(function(f) {
+            f.filters.forEach(function(f2) {
               query.filter.push(f2);
             });
           });
         });
-        viz.destroy(self, {loading: true});
+        viz.destroy(self, {
+          loading: true
+        });
         return viz.initialize(self, self.options);
       });
     }
   }
   if (self.options.query) {
-    viz.fetch(self, self.options.query, function () {
+    viz.fetch(self, self.options.query, function() {
       if (callback && typeof callback === 'function')
         return callback(null, self);
     });
@@ -41582,10 +41585,10 @@ viz.initialize = function (self, options, callback) {
   return self;
 };
 
-viz.lookup = function (base, key) {
+viz.lookup = function(base, key) {
   if (!base)
     return null;
-  var result = _.filter(base, function (item) {
+  var result = _.filter(base, function(item) {
     return item.key === key;
   });
   if (result.length === 0)
@@ -41593,11 +41596,11 @@ viz.lookup = function (base, key) {
   return result[0];
 };
 
-viz.lookupDate = function (base, index) {
+viz.lookupDate = function(base, index) {
   return base[index];
 };
 
-viz.fetch = function (context, query, callback) {
+viz.fetch = function(context, query, callback) {
   if (!context.realtimeQueries)
     context.realtimeQueries = [];
   if (!callback && context && query) {
@@ -41605,31 +41608,32 @@ viz.fetch = function (context, query, callback) {
     query = context;
   }
   var _query = ce.clone(query);
-  viz.stop(context, function () {
+  viz.stop(context, function() {
     if (!Array.isArray(_query)) {
       if (context && context.options && context.options.canvas) {
         context.options.query.interval = context.options.query.interval || context.options.canvas.options.query.interval;
         context.options.query.timeframe = context.options.query.timeframe || context.options.canvas.options.query.timeframe;
       }
-    }
-    else {
+    } else {
       if (context && context.options && context.options.canvas) {
         context.options.query[0].interval = context.options.query[0].interval || context.options.canvas.options.query.interval;
         context.options.query[0].timeframe = context.options.query[0].timeframe || context.options.canvas.options.query.timeframe;
       }
     }
     //adjust offset
-    if (_query.timeframe && typeof _query.timeframe === 'object') {
-      if (_query.timeframe.start)
-        _query.timeframe.start.setHours(_query.timeframe.start.getHours() + joola.timezone(joola.options.timezoneOffset));
-      if (_query.timeframe.end)
-        _query.timeframe.end.setHours(_query.timeframe.end.getHours() + joola.timezone(joola.options.timezoneOffset));
-    }
+    context.options.query.forEach(function(_query) {
+      if (_query.timeframe && typeof _query.timeframe === 'object') {
+        if (_query.timeframe.start)
+          _query.timeframe.start.setHours(_query.timeframe.start.getHours() + joola.timezone(joola.options.timezoneOffset));
+        if (_query.timeframe.end)
+          _query.timeframe.end.setHours(_query.timeframe.end.getHours() + joola.timezone(joola.options.timezoneOffset));
+      }
+    });
     var args = [];
     if (_query.authContext)
       args.push(_query.authContext);
     args.push(_query);
-    args.push(function (err, messages) {
+    args.push(function(err, messages) {
       if (err)
         return callback(err);
 
@@ -41639,13 +41643,13 @@ viz.fetch = function (context, query, callback) {
       if (context.reply)
         context.reply(messages, context.data);
 
-      messages.forEach(function (message, mindex) {
+      messages.forEach(function(message, mindex) {
         if (!context.data)
           context.data = [];
         var _data = [];
         if (context.data[mindex]) {
           _data = context.data[mindex];
-          _data.forEach(function (item) {
+          _data.forEach(function(item) {
             item.state = 'exit';
           });
         }
@@ -41653,7 +41657,7 @@ viz.fetch = function (context, query, callback) {
           joola.logger.debug('fetch took: ' + message.query.ts.duration.toString() + 'ms, results: ' + (message && message.documents ? message.documents.length.toString() : 'n/a'));
         if (message.query.realtimeUID && context.realtimeQueries.indexOf(message.query.realtimeUID) == -1)
           context.realtimeQueries.push(message.query.realtimeUID);
-        message.documents.forEach(function (doc, docindex) {
+        message.documents.forEach(function(doc, docindex) {
           doc = {
             dimensions: {},
             metrics: {},
@@ -41664,12 +41668,12 @@ viz.fetch = function (context, query, callback) {
             realtime: message.query.realtime
           };
           var key = '';
-          message.dimensions.forEach(function (d) {
+          message.dimensions.forEach(function(d) {
             key += doc.raw[d.key];
             doc.dimensions[d.key] = doc.raw[d.key];
             doc.meta[d.key] = d;
           });
-          message.metrics.forEach(function (m) {
+          message.metrics.forEach(function(m) {
             doc.metrics[m.key] = doc.raw[m.key];
             doc.meta[m.key] = m;
           });
@@ -41678,7 +41682,7 @@ viz.fetch = function (context, query, callback) {
           var exist = viz.lookup(_data, doc.key);
           if (exist) {
             var dirty = false;
-            Object.keys(exist.metrics).forEach(function (key) {
+            Object.keys(exist.metrics).forEach(function(key) {
               if (exist.metrics[key] !== doc.metrics[key]) {
                 dirty = true;
                 exist.metrics[key] = doc.metrics[key];
@@ -41688,8 +41692,7 @@ viz.fetch = function (context, query, callback) {
               exist.state = 'update';
             else
               exist.state = 'static';
-          }
-          else {
+          } else {
             _data.push(doc);
           }
         });
@@ -41697,7 +41700,7 @@ viz.fetch = function (context, query, callback) {
       });
 
       var handled = [];
-      context.data[0].forEach(function (data, pointIndex) {
+      context.data[0].forEach(function(data, pointIndex) {
         handled.push(data.key);
         var point = [data];
         var paired = {
@@ -41708,9 +41711,8 @@ viz.fetch = function (context, query, callback) {
           //find the pair
           if (Object.keys(data.dimensions).length === 1 && data.dimensions.timestamp) {
             paired = context.data[1][pointIndex] || paired;
-          }
-          else {
-            paired = _.find(context.data[1], function (item) {
+          } else {
+            paired = _.find(context.data[1], function(item) {
               return item.key === data.key;
             }) || paired;
           }
@@ -41721,13 +41723,11 @@ viz.fetch = function (context, query, callback) {
           if (context.options.enter)
             context.options.enter.apply(context, [point, context.data]);
           context.enter(point, context.data);
-        }
-        else if (data.state === 'update' || paired.state === 'update') {
+        } else if (data.state === 'update' || paired.state === 'update') {
           if (context.options.update)
             context.options.update.apply(context, [point, context.data]);
           context.update(point, context.data);
-        }
-        else if (data.state === 'exit' || paired.state === 'exit') {
+        } else if (data.state === 'exit' || paired.state === 'exit') {
           context.data[0].splice(pointIndex, 1);
           if (context.options.exit)
             context.options.exit.apply(context, [point, context.data]);
@@ -41735,11 +41735,10 @@ viz.fetch = function (context, query, callback) {
         }
       });
       if (context.data.length > 1) {
-        context.data[1].forEach(function (data, pointIndex) {
+        context.data[1].forEach(function(data, pointIndex) {
           if (handled.indexOf(data.key) === -1) {
             var paired = data;
-            var point = [
-              {
+            var point = [{
                 missing: true,
                 meta: paired.meta
               },
@@ -41750,13 +41749,11 @@ viz.fetch = function (context, query, callback) {
               if (context.options.enter)
                 context.options.enter.apply(context, [point, context.data]);
               context.enter(point, context.data);
-            }
-            else if (data.state === 'update' || paired.state === 'update') {
+            } else if (data.state === 'update' || paired.state === 'update') {
               if (context.options.update)
                 context.options.update.apply(context, [point, context.data]);
               context.update(point, context.data);
-            }
-            else if (data.state === 'exit' || paired.state === 'exit') {
+            } else if (data.state === 'exit' || paired.state === 'exit') {
               context.data[1].splice(pointIndex, 1);
               if (context.options.exit)
                 context.options.exit.apply(context, [point, context.data]);
@@ -41766,7 +41763,7 @@ viz.fetch = function (context, query, callback) {
         });
       }
       if (context.options.done)
-        context.options.done.apply(context, [context.data,messages]);
+        context.options.done.apply(context, [context.data, messages]);
       if (context.done)
         context.done(context.data, messages);
       return callback(null, context.data);
@@ -41777,27 +41774,25 @@ viz.fetch = function (context, query, callback) {
   });
 };
 
-viz.stop = function (self, callback) {
+viz.stop = function(self, callback) {
   if (!callback)
-    callback = function () {
-    };
+    callback = function() {};
   if (self.realtimeQueries) {
-    async.map(self.realtimeQueries, function (q, callback) {
+    async.map(self.realtimeQueries, function(q, callback) {
       joola.logger.debug('Stopping realtime query [' + q + '].');
       joola.query.stop(q, callback);
-    }, function (err) {
+    }, function(err) {
       if (err)
         return callback(err);
       return callback(null);
     });
-  }
-  else
+  } else
     return callback(null);
 };
 
-viz.destroy = function (self, vizOptions) {
+viz.destroy = function(self, vizOptions) {
   if (self.realtimeQueries) {
-    self.realtimeQueries.forEach(function (q) {
+    self.realtimeQueries.forEach(function(q) {
       joola.logger.debug('Stopping realtime query [' + q + '].');
       joola.query.stop(q);
     });
